af90f6d5c427ccd8c7d444177233f62f0a169540
Bango-TECH <gpsghondage@gmail.com>
2024-07-19 12:42:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 
 let mongoConnected = false;
+let mongoError = '';
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     })
     .catch((err) => {
         console.error("MongoDB connection error:", err);
+        mongoError = err.message;
         mongoConnected = false;
     });
 
@@ -39,7 +41,7 @@ app.get('/', (req, res) => {
     if (mongoConnected) {
         res.send("MongoDB is connected successfully.");
     } else {
-        res.send("Failed to connect to MongoDB.");
+        res.send(`Failed to connect to MongoDB. Error: ${mongoError}`);
     }
 });
 
